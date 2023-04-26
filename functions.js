b203ff9b0069625e483614ee41d92c44411980a5
johnnyyork13 <johnnyyork13@hotmail.com>
2023-04-26 15:30:06 -0400

    add dictionary API and styles

---
+++ functions.js
@@ -0,0 +1,48 @@
+function updateCell(btn) {
+    let td = btn.parentElement;
+    let points = score.value;
+    td.children[0].textContent = points;
+    score.value = '';
+    updateScoreboard();
+}
+
+const showScoreDiv = document.getElementById('showScoreDiv');
+let addPlayersToScoreboard = false;
+function updateScoreboard() {
+    const nameInput = document.getElementsByClassName('nameInput');
+    if (!addPlayersToScoreboard) {
+        for (let i = 0; i < nameInput.length; i++) {
+            let e = nameInput[i];
+            console.log(e.value);
+            let playerContainer = document.createElement('div');
+            let newPlayerName = document.createElement('p');
+            let newPlayerScore = document.createElement('p');
+            newPlayerName.textContent = `${e.value}'s Score: `;
+            newPlayerScore.textContent = 0;
+            playerContainer.appendChild(newPlayerName);
+            playerContainer.appendChild(newPlayerScore);
+            showScoreDiv.appendChild(playerContainer);
+        }
+        addPlayersToScoreboard = true;
+    } else {
+        for (let i = 0; i < nameInput.length; i++) {
+            let scoreCount = 0;
+            for (let x = 0; x < tbody.children.length; x++) {
+                let turnScore = Number(tbody.children[x].children[i+1].children[0].textContent);
+                scoreCount += turnScore;
+            }
+            showScoreDiv.children[i].children[1].textContent = scoreCount;
+        }
+    }
+}
+
+async function getWord(word) {
+    try {
+        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+        const responseData = await response.json();
+        const wordMeaning = responseData[0].meanings[0].definitions[0].definition;
+        definition.textContent = `${word}:  ${wordMeaning}`;
+    } catch (err) {
+        definition.textContent = 'Not a valid word.';
+    }
+}
---
+++ index.js
@@ -0,0 +1,103 @@
+const setup = document.getElementById('setup');
+const playerCountInput = document.getElementById('playerCountInput');
+const playerCountBtn = document.getElementById('playerCountBtn');
+const playerNamesContainer = document.getElementById('playerNamesContainer');
+const game = document.getElementById('game');
+const score = document.getElementById('score');
+const thead = document.getElementById('gameHeader');
+const tbody = document.getElementById('gameBody');
+const nextTurnBtn = document.getElementById('nextTurn');
+const definitionBtn = document.getElementById('definitionBtn');
+const wordBox = document.getElementById('wordBox');
+const definition = document.getElementById('definition');
+
+let players = [];
+
+playerCountBtn.addEventListener('click', function() {
+    if (playerCountInput.value <= 4 && playerCountInput.value >= 1) {
+        for (let i = 0; i < playerCountInput.value; i++) {
+            playerNamesContainer.style.visibility = 'visible';
+            const nameContainer = document.createElement('div');
+            const nameLabel = document.createElement('label');
+            const nameInput = document.createElement('input');
+            nameContainer.classList.add('nameBox');
+            nameInput.classList.add('nameInput');
+            nameLabel.textContent = `Player ${i+1} Name:`;
+            nameContainer.appendChild(nameLabel);
+            nameContainer.appendChild(nameInput);
+            playerNamesContainer.appendChild(nameContainer);
+        }
+        const playerNamesBtn = document.createElement('button');
+        playerNamesBtn.textContent = 'Start Game';
+        playerNamesContainer.appendChild(playerNamesBtn);
+        playerNamesBtn.addEventListener('click', function() {
+            const nameInput = document.getElementsByClassName('nameInput');
+            let nameCheck = true;
+            players = [];
+            for (let i = 0; i < nameInput.length; i++) {
+                const e = nameInput[i];
+                if (e.value === '') {
+                    nameCheck = false;
+                } else {
+                    players.push(e.value);
+                }
+            }
+            if (nameCheck) {
+                setup.style.visibility = 'hidden';
+                setup.style.height = '100px';
+                playerNamesContainer.style.visibility = 'hidden';
+                game.style.visibility = 'visible';
+                //add to table now
+                const turnHeader = document.createElement('th');
+                turnHeader.textContent = 'Turn#';
+                thead.appendChild(turnHeader);
+                for (let i = 0; i < nameInput.length; i++) {
+                    const newPlayer = document.createElement('th');
+                    newPlayer.textContent = nameInput[i].value;
+                    thead.appendChild(newPlayer);
+                }
+                updateScoreboard();
+                addNewRow(nameInput.length);
+            } else {
+                alert('Please fill out all name fields!');
+            }
+            
+        })
+    } else {
+        alert('Please enter a player count between 1 and 4!');
+    }
+})
+
+
+
+function addNewRow(numPlayers) {
+    const tr = document.createElement('tr');
+    const rowNum = document.createElement('td');
+    rowNum.textContent = tbody.children.length;
+    tr.appendChild(rowNum);
+    for (let i = 0; i < numPlayers; i++) {
+        const td = document.createElement('td');
+        const p = document.createElement('p');
+        const tdBtn = document.createElement('button');
+        p.textContent = '0';
+        tdBtn.textContent = 'Add';
+        td.appendChild(p);
+        td.appendChild(tdBtn);
+        tr.appendChild(td);
+        tdBtn.addEventListener('click', function() {
+            updateCell(this);
+            td.appendChild(tdBtn);
+        })
+    }
+    tbody.appendChild(tr);
+
+}
+
+nextTurnBtn.addEventListener('click', function() {
+    const nameInput = document.getElementsByClassName('nameInput');
+    addNewRow(nameInput.length);
+})
+
+definitionBtn.addEventListener('click', function() {
+    getWord(wordBox.value);
+})
